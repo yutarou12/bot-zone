7e0ff64666263a2cdb4f2a4cd39aceff8cfd7273
yutarou12 <sasaki.yuutarou12@gmail.com>
2021-05-05 19:05:32 +0900

    add チャンネルの再生成コマンドの追加

--- cogs/channels.py
+++ cogs/channels.py
@@ -1,4 +1,5 @@
 import discord
+import asyncio
 from discord.ext import commands
 
 
@@ -16,15 +17,11 @@ class Channels(commands.Cog):
         category_private = guild.get_channel(self.category_private_id)
 
         if role_bot and category_private:
-            print(0)
             if ctx.channel.id == self.channel_private_id:
-                print(1)
                 if f'room-{ctx.author.name}' in [ch.name for ch in guild.text_channels]:
-                    print(2)
                     e_msg = discord.Embed(title=f'チャンネルは既に作成されています')
                     await ctx.reply(embed=e_msg, allowed_mentions=discord.AllowedMentions.none())
                 else:
-                    print(3)
                     overwrites = {
                         guild.default_role: discord.PermissionOverwrite(view_channel=False),
                         ctx.author: discord.PermissionOverwrite(view_channel=True),
@@ -35,6 +32,42 @@ class Channels(commands.Cog):
                                                               category=category_private)
                     s_msg = discord.Embed(title='プライベートチャンネルを作成しました', description=f'チャンネル: {channel.mention}')
                     await ctx.reply(embed=s_msg, allowed_mentions=discord.AllowedMentions.none())
+
+    @commands.command()
+    async def clean(self, ctx):
+        guild = ctx.guild
+        category_private = guild.get_channel(self.category_private_id)
+
+        if category_private:
+            if ctx.channel.id == self.channel_private_id:
+                user_channel = [ch for ch in guild.text_channels if ch.name == f'room-{ctx.author.name}']
+                if user_channel:
+                    e_msg = discord.Embed(title=f'チャンネの再生成',
+                                          description="再生成する場合は`y`、キャンセルする場合は`n`を送信してください")
+                    re_msg = await ctx.reply(embed=e_msg, allowed_mentions=discord.AllowedMentions.none())
+
+                    def check(message):
+                        if message.author == ctx.author and (message.content in ["y", "n"]):
+                            return message.content
+                    try:
+                        msg = await self.bot.wait_for('message', timeout=15.0, check=check)
+                    except asyncio.TimeoutError:
+                        await re_msg.edit(discord.Embed(description='時間切れです'))
+
+                    if msg.content == 'y':
+                        await msg.delete()
+                        new_channel = await user_channel[0].clone(name=f'room-{ctx.author.name}')
+                        await user_channel[0].delete()
+                        await re_msg.edit(embed=discord.Embed(title='再生成しました',
+                                                              description=f'チャンネル: {new_channel.mention}'))
+                    elif msg.content == 'n':
+                        await msg.delete()
+                        await re_msg.edit(embed=discord.Embed(description='キャンセルしました'))
+                    else:
+                        pass
+                else:
+                    await ctx.reply(embed=discord.Embed(description="プライベートチャンネルが見つかりません"),
+                                    allowed_mentions=discord.AllowedMentions.none())
 
 
 def setup(bot):
